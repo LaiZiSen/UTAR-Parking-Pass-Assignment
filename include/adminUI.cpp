374cabdbbcb934c0d2c6f8f2f75e2fa532e603ab
LaiZiSen <samlai060504@gmail.com>
2026-04-08 00:42:35 +0800

    Forgot to add the files for admin ui and approve applications

---
+++ include/adminUI.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include "../header/txtDB.h"
+#include "../header/txtPath.h"
+#include "../header/approveAPP.h"
+
+using namespace std;
+
+
+void printAdminMenu() {
+    cout << endl;
+    cout << "Menu" << endl;
+    cout << "1. Approve Applications" << endl;
+    cout << "2. Apply Applications" << endl;
+    cout << "3. Check Analytics" << endl;
+    cout << "4. Logout" << endl << endl;
+}
+
+char getOption(){
+    cout << "Enter a number for your option:  ";
+    
+    char output;
+    cin >> output;
+
+    return output;
+}
+
+void adminUI(admin adminData) {
+    cout << endl << "WELCOME" << adminData.name << endl;
+
+    bool loggedIn = true;
+    while(loggedIn) {
+        char option;
+        
+        printAdminMenu();
+        option = getOption();
+
+        switch (option) {
+            case '1':
+                approveAPP();
+                break;
+            case '2':
+                cout << endl << "Check Analytics" << endl;
+                break;
+            case '3':
+                cout << endl << "Apply Applications" << endl;
+                break;
+            case '4':
+                cout << endl << "Logging out " << adminData.name << endl;
+                loggedIn = false;
+                break;
+            default:
+                cout << "Invalid option, enter another number" << endl;
+                break;
+        };
+    }
+}
+
+int main () {
+
+    fstream adminFile(ADMIN_FILE);
+
+    admin adminData;
+    getAdmin(adminFile, adminData, 0);
+
+    adminUI(adminData);
+
+    return 0;
+}
---
+++ include/approveAPP.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../header/txtDB.h"
+#include "../header/txtPath.h"
+
+using namespace std;
+
+void displayAPP (application applicationObj) {
+    cout << "Username    - " << applicationObj.username << endl;
+    cout << "Application - " << applicationObj.pass_application << endl;
+    cout << "Car Plate   - " << applicationObj.car_plate << endl;
+    cout << "Appli type  - " << applicationObj.type << endl;
+    cout << "Appli date  - " << applicationObj.date << endl;
+}
+
+void displayApvMenu() {
+    cout << endl << "APPROVE OR REJECT?" << endl;
+    cout << "(1) APV" << endl;
+    cout << "(2) RJT" << endl;
+}
+
+void displayContApvMenu() {
+    cout << endl << "NEXT APPLICATION or EXIT?" << endl;
+    cout << "(1) NEXT" << endl;
+    cout << "(2) EXIT" << endl;
+}
+
+float getRFDAmount(fstream &transFile, application applicationData) {
+
+    transaction transactionObj;
+    int lineNum;
+    while (!transFile.eof())
+    {
+        getTransaction(transFile, transactionObj, lineNum);
+
+        if (transactionObj.username.compare(applicationData.username) == 0) {
+            return transactionObj.amount;
+        }
+    }
+    return 0;
+}
+
+void approve(fstream &file, application applicationData) {
+    edit(file, applicationData.line, applicationData.lineSize, applicationData.status_Attr, appStatusAPV);
+}
+
+void reject(fstream &file, application applicationData) {
+    edit(file, applicationData.line, applicationData.lineSize, applicationData.status_Attr, appStatusRJT);
+    
+    fstream transFile(TRANSACTION_FILE);
+
+    transaction rfdTransaction;
+    rfdTransaction.username = applicationData.username;
+    rfdTransaction.amount = getRFDAmount(transFile, applicationData); //later
+    rfdTransaction.date = getCurrYYMMDD();
+    rfdTransaction.trans_type = transTypeRFD;
+
+    writeTransaction(transFile, rfdTransaction);
+    transFile.close();
+}
+
+void approveAPP() {
+    cout << "Approve applications !" << endl;
+
+    fstream applicationFile(APPLICATION_FILE);
+    int lineNum = 0;
+    application applicationObj;
+
+    bool continueAPV = true;
+    while(!applicationFile.eof() && continueAPV) {
+
+        applicationFile.seekp(applicationObj.lineSize * lineNum);
+        RESULT getAPPResult = getApplication(applicationFile, applicationObj, lineNum);
+        lineNum++;
+
+        bool isPND = (applicationObj.status.compare(appStatusPND) == 0) ? true : false;
+
+        if (getAPPResult != VALID_RECORD || !isPND) {continue;}
+        
+        displayAPP(applicationObj);
+
+        bool done = false;
+        char choice;
+
+        displayApvMenu();
+        while (!done){
+            cout << "Enter choice:  ";
+            cin >> choice;
+            switch (choice){
+            case '1':
+                //approve
+                cout << "Approve " << applicationObj.username << endl;
+
+                approve(applicationFile ,applicationObj);
+
+                done = true; break;
+            case '2':
+                //reject
+                cout << "Reject " << applicationObj.username << endl;
+                
+                reject(applicationFile, applicationObj);
+
+                done = true; break;
+            default:
+                cout << "Invalid choice, enter another number!";
+                break;
+            }
+        }
+
+        done = false;
+        displayContApvMenu();
+        while (!done){
+            cout << "Enter choice:  ";
+            cin >> choice;
+
+            switch (choice)
+            {
+            case '1':
+                // Continue to approve application
+                done = true;
+                break;
+            case '2':
+                // Exit back to Admin UI
+                done = true;
+                continueAPV = false; 
+                break;
+            default:
+                cout << "Invalid choice, enter another number!";
+                break;
+            }
+        }
+    }
+
+    if (applicationFile.eof()) {
+        cout << endl << "End of PENDING Applications" << endl;
+    }
+
+    cout << endl << "Exiting Application Approve" << endl;
+    applicationFile.close();
+}
