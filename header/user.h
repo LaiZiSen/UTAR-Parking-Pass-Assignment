91d1a9a61e74f47ee28519a99e2e7e7cf981ce1a
srrouuuu <srlim2006@gmail.com>
2026-04-08 00:21:33 +0800

    add user header

---
+++ header/user.h
@@ -0,0 +1,11 @@
+#ifndef USERFUNCTION_H
+#define USERFUNCTION_H
+
+#include <string>
+using namespace std;
+
+// Function declarations (prototypes only)
+string userLogin();
+void userUI(string username);
+
+#endif
