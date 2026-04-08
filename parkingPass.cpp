80314913bd380b3d8fb97e61f97775b92e87385b
LaiZiSen <samlai060504@gmail.com>
2026-04-08 16:00:03 +0800

    Made the base for parkingPass and added txtPath.h, (prob shouldn't add txtPath but fuck it)

--- parkingPass.cpp
+++ parkingPass.cpp
@@ -4,6 +4,60 @@
 
 using namespace std;
 
+void printGeneralMenu() {
+    cout << endl;
+    cout << "General Menu" << endl;
+    cout << "1. Description" << endl;
+    cout << "2. Login as User" << endl;
+    cout << "3. Login as Admin" << endl;
+    cout << "4. Register" << endl; 
+    cout << "5. Exit Program" << endl;
+}
+
+char getChoice(){
+    cout << "Enter a number for your option:  ";
+    
+    char output;
+    cin >> output;
+
+    return output;
+}
+
+void printDescription() {
+    cout << endl << "Description" << endl;
+}
+
 int main () {
-    return 0;
+    
+    bool progRunning = true;
+
+    char choice;
+    while(progRunning) {
+        printGeneralMenu();
+        choice = getChoice();
+
+        switch (choice) {
+            case '1':
+                printDescription();
+                break;
+            case '2':
+                // Login as User
+                break;
+            case '3':
+                // Login as Admin
+                break;
+            case '4':
+                // Register user account
+                break;
+            case '5':
+                cout << endl << "Exiting Parking Pass program";
+                progRunning = false;
+                break;
+            default:
+                cout << "Invalid option, enter anotehr number" << endl;
+                break;
+        }
+
+    }
+
 }
